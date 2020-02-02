b102ddf84485ca1c7320273d72996ac6953abd14
William McCall <wnm.code@gmail.com>
2020-02-02 13:33:17 -0500

    Begin endpoints

--- main.js
+++ main.js
@@ -1,39 +1,69 @@
-const cities = require("cities");
 const url = require("url");
 const http = require("http");
 
-// Create a server object
-const app = http.createServer(function(req, res) {
-  // http header
-  res.writeHead(200, { "Content-Type": "text/html" });
+var args = process.argv.slice(2);
+const DEBUG = args.length > 0 && args[0] === "debug";
 
-  var browserUrl = req.url;
-  var urlArray = browserUrl.split("/");
+const log = message => {
+  DEBUG && console.log(message);
+};
+
+const getUrlArray = url => {
+  var urlArray = url.split("/");
   urlArray.shift();
   urlArray = urlArray.map(el => {
     return el.split("?")[0];
   });
-  console.log(urlArray);
+  return urlArray;
+};
 
-  if (urlArray[0] === "about") {
-    res.write(" Welcome to about us page");
-    res.end();
-  } else if (urlArray[0] === "contact") {
-    res.write(" Welcome to contact us page");
-    res.end();
-  } else if (urlArray[0] === "cities") {
-    var city, query;
+// Create a server object
+const app = http.createServer(function(req, res) {
+  log(req.url);
+  log(req.headers);
+  var browserUrl = req.url;
+  var urlArray = getUrlArray(browserUrl);
+  log(urlArray);
+
+  try {
+    var query;
     query = url.parse(browserUrl, true).query;
-    console.log(query);
-    if (query.zipCode) city = cities.zip_lookup(query.zipCode).city;
-    else city = "not found";
+    log(`query:`);
+    log(query);
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`&lt;h1&gt;The city you are in is ${city}.&lt;/h1&gt;`);
-    res.end();
-  } else {
-    res.write("Hello World!");
-    res.end();
+
+    switch (urlArray[0]) {
+      case "createGame":
+        res.write(`done`);
+        break;
+      case "joinGame":
+        res.write(`done`);
+        break;
+      case "pickupDeck":
+        res.write(`done`);
+        break;
+      case "pickupDiscard":
+        res.write(`done`);
+        break;
+      case "playCard":
+        res.write(`done`);
+        break;
+      case "discard":
+        res.write(`done`);
+        break;
+      case "rummy":
+        res.write(`done`);
+        break;
+      default:
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("Not found");
+    }
+  } catch (err) {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("Malformed Request");
   }
+
+  res.end();
 });
 
 app.listen(3000, function() {
