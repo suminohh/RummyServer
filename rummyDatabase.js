a1b5c9fc47391e71be59c10a512c3a15100b9f23
William McCall <wnm.code@gmail.com>
2020-02-08 01:50:08 -0500

    Clean up methods, add game exists check for joining

--- rummyDatabase.js
+++ rummyDatabase.js
@@ -24,43 +24,29 @@ module.exports = class RummyDatabase {
   };
 
   getDeckRef = async gameRef => {
-    var deckRef;
-    await gameRef
-      .collection("deck")
-      .get()
-      .then(querySnapshot => {
-        var deckQueryDocumentSnapshot = querySnapshot.docs[0];
-        deckRef = deckQueryDocumentSnapshot.ref;
-      });
-    return deckRef;
+    var querySnapshot = await gameRef.collection("deck").get();
+    var deckQueryDocumentSnapshot = querySnapshot.docs[0];
+    return deckQueryDocumentSnapshot.ref;
   };
 
   getHandsRefs = async gameRef => {
-    var handsRefs;
-    await gameRef
-      .collection("hands")
-      .get()
-      .then(querySnapshot => {
-        handsRefs = querySnapshot.docs;
-      });
-    return handsRefs;
+    var querySnapshot = await gameRef.collection("hands").get();
+    return querySnapshot.docs;
   };
 
   createGame = async userID => {
     const userRef = await this.getUserRef(userID);
     var gameID = "MISSING";
-    await this.db
+    var gameRef = await this.db
       .collection("games")
-      .add({ player1: userRef, turn: userRef })
-      .then(async gameRef => {
-        gameID = gameRef.id;
-        const deck = new Deck();
-        deck.shuffle();
-        gameRef
-          .collection("deck")
-          .add({ cards: deck.cards, cards_used: deck.cardsUsed });
-        gameRef.collection("hands").add({ player: userRef, cards: [] });
-      });
+      .add({ player1: userRef, turn: userRef });
+    gameID = gameRef.id;
+    const deck = new Deck();
+    deck.shuffle();
+    gameRef
+      .collection("deck")
+      .add({ cards: deck.cards, cards_used: deck.cardsUsed });
+    gameRef.collection("hands").add({ player: userRef, cards: [] });
     return gameID;
   };
 
@@ -69,41 +55,46 @@ module.exports = class RummyDatabase {
     const userRef = await this.getUserRef(userID);
     const gameRef = this.getGameRef(gameID);
     // TODO: fail if player2 is already set
-    await gameRef.update({ player2: userRef }).then(async _ => {
-      await gameRef
-        .collection("hands")
-        .add({ player: userRef, cards: [] })
-        .then(async _ => {
-          await this.getDeckRef(gameRef).then(async deckRef => {
-            await deckRef.get().then(async deckDoc => {
-              var cards = deckDoc.data().cards;
-              var first14Cards = cards.slice(0, 14);
-              var p1Hand = [];
-              var p2Hand = [];
-              for (var i = 0; i < 14; i += 2) {
-                p1Hand.push(first14Cards[i]);
-                p2Hand.push(first14Cards[i + 1]);
-              }
-              await this.getHandsRefs(gameRef).then(async handsRefs => {
-                var hand1 = await handsRefs[0].ref.get();
-                if (hand1.data().player == userRef) {
-                  handsRefs[0].ref.update({ cards: p2Hand });
-                  handsRefs[1].ref.update({ cards: p1Hand });
-                } else {
-                  handsRefs[0].ref.update({ cards: p1Hand });
-                  handsRefs[1].ref.update({ cards: p2Hand });
+    await gameRef
+      .update({ player2: userRef })
+      .then(async _ => {
+        await gameRef
+          .collection("hands")
+          .add({ player: userRef, cards: [] })
+          .then(async _ => {
+            await this.getDeckRef(gameRef).then(async deckRef => {
+              await deckRef.get().then(async deckDoc => {
+                var cards = deckDoc.data().cards;
+                var first14Cards = cards.slice(0, 14);
+                var p1Hand = [];
+                var p2Hand = [];
+                for (var i = 0; i < 14; i += 2) {
+                  p1Hand.push(first14Cards[i]);
+                  p2Hand.push(first14Cards[i + 1]);
                 }
-              });
+                await this.getHandsRefs(gameRef).then(async handsRefs => {
+                  var hand1 = await handsRefs[0].ref.get();
+                  if (hand1.data().player == userRef) {
+                    handsRefs[0].ref.update({ cards: p2Hand });
+                    handsRefs[1].ref.update({ cards: p1Hand });
+                  } else {
+                    handsRefs[0].ref.update({ cards: p1Hand });
+                    handsRefs[1].ref.update({ cards: p2Hand });
+                  }
+                });
 
-              var firstDiscard = cards[14];
-              gameRef.update({ discard: [firstDiscard] });
-              await deckRef.update({ cards_used: 15 }).then(async _ => {
-                returnMessage = "Success";
+                var firstDiscard = cards[14];
+                gameRef.update({ discard: [firstDiscard] });
+                await deckRef.update({ cards_used: 15 }).then(async _ => {
+                  returnMessage = "Success";
+                });
               });
             });
           });
-        });
-    });
+      })
+      .catch(_ => {
+        returnMessage = "Game not Found";
+      });
     return returnMessage;
   };
 
